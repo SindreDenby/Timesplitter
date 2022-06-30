550433bb564689475cc2361a54873d892b34c138
Sindre Denby <sidenby1@gmail.com>
2022-06-30 15:15:32 +0200

    first commit

---
+++ configurator.py
@@ -0,0 +1,147 @@
+import json
+import tkinter as tk
+import tkinter.messagebox
+import os
+
+project_types = ['projects.json','admin.json', 'ekstern.json', 'intern.json', 'fastpris.json']
+appdata_dir = (os.getenv('APPDATA')).replace("\\", "/") + "/Timesplitter/config/"
+
+def create_listBox(box, row, col):
+    listboxFrame = tk.Frame(box)
+    listboxFrame.grid(row=row, column=col)
+
+    listbox = tk.Listbox(listboxFrame, width=40, height=30)
+
+    listbox.pack(side = tk.LEFT, fill = tk.BOTH)
+
+    scrollbar = tk.Scrollbar(listboxFrame)
+
+    scrollbar.pack(side = tk.RIGHT, fill = tk.BOTH)
+        
+    listbox.config(yscrollcommand = scrollbar.set)
+
+    scrollbar.config(command = listbox.yview)
+
+    return listbox
+
+def insert_into_listbox(listbox: tk.Listbox, list):
+    listbox.delete(0, tk.END)
+
+    for i in list:
+        listbox.insert(tk.END, i)
+
+class config_ui:
+    def __init__(self):
+
+        self.projects = self.read_files()
+
+        self.projects[0]['projects'] = self.get_unselected_projects()
+
+        root = tk.Tk()
+
+        curCol = 0
+
+        mainFrame = tk.Frame(root, padx=30, pady=30)
+        mainFrame.grid()
+
+        self.project_list_boxes = []
+        for i, project in enumerate(self.projects):
+            tk.Label(mainFrame,
+                text= project['name'].split('.')[0]
+            ).grid(row=0, column=curCol)
+
+            self.project_list_boxes.append(create_listBox(mainFrame, 1, curCol))
+
+            tk.Button(mainFrame, 
+                command= lambda i = i: self.move_selected(i),
+                text="Flytt hit"
+            ).grid(row=2, column=curCol)
+            
+            curCol += 1
+
+        tk.Button(mainFrame,
+            command=self.save_config,
+            text="Save"
+        ).grid(row= 1, column=curCol)
+
+        self.update_lists()
+        root.mainloop()
+
+    def read_files(self):
+        """
+        Leser av posjekt filene og returnerer dem som en dict
+        """
+        loaded_project_types = []
+
+        for i in project_types:
+            f = open(appdata_dir + i, 'r')
+            loaded_project_types.append({
+                'name': i,
+                'projects': json.loads(f.read())
+            })
+            f.close()
+        return loaded_project_types
+
+    def get_selected_element(self):
+        """
+        Returnerer: (listboxNr: int, listBoxIndex: int)
+        """
+
+        for listboxNr, listbox in enumerate(self.project_list_boxes) :
+            for selected in listbox.curselection():
+                return (listboxNr, selected)
+
+
+    def move_selected(self, box_index):
+        selectedElement = self.get_selected_element()
+        
+        self.projects[box_index]['projects'].append(
+            self.projects[selectedElement[0]]['projects'][selectedElement[1]]
+        )
+
+        self.projects[selectedElement[0]]['projects'].remove(
+            self.projects[selectedElement[0]]['projects'][selectedElement[1]]
+        )
+
+
+        self.update_lists()
+
+    def update_lists(self):
+        for i, listbox in enumerate(self.project_list_boxes):
+            insert_into_listbox(listbox, self.projects[i]['projects'])
+        
+    def save_config(self):
+        for projectNr in range(len(self.projects) - 1):
+            f = open(appdata_dir + self.projects[projectNr + 1]['name'], 'w')
+            f.write(json.dumps(self.projects[projectNr + 1]['projects'], indent=2))
+            f.close()
+
+        self.projects = self.read_files()
+
+        self.projects[0]['projects'] = self.get_unselected_projects()
+
+        self.update_lists()
+        tkinter.messagebox.showinfo("Lagret", "Config er lagret")
+
+    def get_unselected_projects(self):
+        unselected_projects = self.projects[0]['projects'].copy()
+
+        selected_projects = []
+
+        for i in range(4):
+            selected_projects.extend(self.projects[i + 1]['projects'])
+
+       
+        for project in self.projects[0]['projects']:
+   
+            if project in selected_projects:
+                unselected_projects.remove(project)
+                    
+        return unselected_projects
+
+
+def main():
+    config_ui()
+
+if __name__ == '__main__':
+    main()
---
+++ hub.py
@@ -0,0 +1,116 @@
+import os
+import tkinter as tk
+import tkinter.messagebox
+import configurator
+import timesplitter
+from tkinter import filedialog
+
+appdata_dir = (os.getenv('APPDATA')).replace("\\", "/") + "/Timesplitter/config/"
+
+"""
+Use: py -3.9-64 -m PyInstaller -w hub.py
+"""
+
+class Hub_UI: 
+    def __init__(self):
+
+        root = tk.Tk()
+        
+        mainFrame = tk.Frame(root, padx=30, pady=30)
+        mainFrame.grid()
+
+        curCol = 0
+
+        # Ansatte Btn
+        tk.Button(mainFrame,
+            text="Lag timer for ansatte",
+            command= lambda: timesplitter.reformat_into_employees(self.get_file_save_dir(), self.get_file_dir())
+        ).grid(row=0, column= curCol)
+
+        # Prosjekter Btn
+        tk.Button(mainFrame,
+            text="Lag timer for prosjekter",
+            command= lambda: timesplitter.reformat_into_projects(self.get_file_save_dir(), self.get_file_dir())
+        ).grid(row=1, column= curCol)
+
+        # Config Btn
+        tk.Button(mainFrame,
+            text="Config",
+            command=configurator.main,
+            bg="#4287f5"
+        ).grid(row=2, column=curCol)
+
+        curCol += 1
+
+        self.setFileBtn = tk.Button(mainFrame,
+            text="Set file",
+            command=self.set_file,
+            bg= "#03fca9"
+        )
+        self.setFileBtn.grid(row=0, column=curCol)
+
+        self.setDirBtn = tk.Button(mainFrame,
+            text="Set Save Directory",
+            command=self.set_save_dir,
+            bg= "#03fc6b"
+        )
+        self.setDirBtn.grid(row=1, column= curCol)
+
+        fileNameFrame = tk.Frame(mainFrame)
+        fileNameFrame.grid(row=2, column=curCol)
+        fileNameFrame.grid()
+
+        # File name label
+        tk.Label(fileNameFrame, text="Filnavn:").grid(row=0, column=0)
+
+        self.fileNameEntry = tk.Entry(fileNameFrame, justify=tk.RIGHT)
+        self.fileNameEntry.grid(row=0, column=1)
+
+        tk.Label(fileNameFrame, text=".xlsx").grid(row=0, column=2)
+
+
+        root.mainloop()
+
+    def set_save_dir(self):
+        dir = filedialog.askdirectory()
+        if dir != "":
+            self.saveDir = dir
+            self.setDirBtn.config(text=self.saveDir)
+
+    def set_file(self):
+        file = filedialog.askopenfilename()
+        if file != "":
+            self.fileDir = file
+            self.setFileBtn.config(text=self.fileDir)
+
+    def get_file_save_dir(self):
+        try:
+            return f'{self.saveDir}/{self.fileNameEntry.get()}.xlsx'
+        except AttributeError: 
+            tkinter.messagebox.showinfo("File error", "Fil og/eller lagrinspunkt er ikke valgt")
+
+    def get_file_dir(self):
+        try:
+            return self.fileDir
+        except AttributeError: 
+            tkinter.messagebox.showinfo("File error", "Fil og/eller lagrinspunkt er ikke valgt")
+
+project_types = ['projects.json', 'admin.json', 'ekstern.json', 'intern.json', 'fastpris.json']
+
+def first_time_setup():
+    print("First time setup")
+    os.makedirs(appdata_dir, exist_ok=True)
+
+    for type in project_types:
+        with open(appdata_dir + type, "w+") as f:
+            f.write("[]")
+
+def main():
+    if not os.path.exists(appdata_dir): 
+        first_time_setup()
+        
+    Hub_UI()
+    pass
+
+if __name__ == '__main__':
+    main()
