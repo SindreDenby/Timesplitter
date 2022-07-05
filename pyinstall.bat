ae6e9411ff32a7821666e6559d3d6f83bde5088d
Sindre Denby <sidenby1@gmail.com>
2022-07-05 15:54:32 +0200

    install config

--- pyinstall.bat
+++ pyinstall.bat
@@ -3,6 +3,8 @@
 :: 2. Run this file (Python 3.9 with Pyinstaller and inno compiler must be installed!)
 :: 3. Installer file is in ./Output
 
+set ISCCInstall="C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
+
 py -3.9-64 -m PyInstaller -y -i "adigo icon.png" --distpath ./pyinstoutput/dist --workpath ./pyinstoutput/build --clean -w hub.py
 
-"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" ".\inno setup v 1.iss"
+%ISCCInstall% ".\inno setup v 1.iss"
