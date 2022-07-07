a6c3103d1d15827aeb50b55036a0de523f30dec6
Sindre Denby <sidenby1@gmail.com>
2022-07-07 13:09:46 +0200

    theme vista

--- hub.py
+++ hub.py
@@ -5,7 +5,7 @@ import configurator
 import csv_splitter
 import adigo_icon
 import tooltip
-from tkinter import filedialog
+from tkinter import filedialog, ttk
 
 appdata_dir = (os.getenv('APPDATA')).replace("\\", "/") + "/Timesplitter/config/"
 
@@ -17,6 +17,10 @@ class Hub_UI:
 
         adigo_icon.set_base64_icon(adigo_icon.icon, root)
 
+        style = ttk.Style(root)
+
+        style.theme_use('vista')
+
         mainFrame = tk.Frame(root, padx=30, pady=30)
         mainFrame.grid()
 
@@ -25,34 +29,32 @@ class Hub_UI:
         setFileFrame.grid(row=0, column=0, columnspan= 2)
         setFileFrame.grid()
 
-        tk.Label(setFileFrame, text=".csv fil:").grid(row=0, column=0)
+        ttk.Label(setFileFrame, text=".csv fil:").grid(row=0, column=0)
 
-        self.csvFileInput = tk.Entry(setFileFrame, justify=tk.RIGHT, width=40)
+        self.csvFileInput = ttk.Entry(setFileFrame, justify=tk.RIGHT, width=40)
         self.csvFileInput.grid(row=0, column=1)
 
-        tk.Button(setFileFrame,
+        ttk.Button(setFileFrame,
             text="...",
             command=self.set_file,
+            width=6
         ).grid(row=0, column=2)
 
-        tk.Label(setFileFrame, text="Lagres som:").grid(row=1, column=0)
+        ttk.Label(setFileFrame, text="Lagres som:").grid(row=1, column=0)
 
-        self.saveDirInput = tk.Entry(setFileFrame, justify=tk.RIGHT, width=40)
+        self.saveDirInput = ttk.Entry(setFileFrame, justify=tk.RIGHT, width=40)
         self.saveDirInput.grid(row=1, column=1)
 
-        tk.Button(setFileFrame,
+        ttk.Button(setFileFrame,
             text="...",
             command=self.set_save_dir,
+            width=6
         ).grid(row=1, column=2)
 
         # Config Btn
-        tk.Button(setFileFrame,
-            text="Config",
-            command=configurator.main,
-            bg="#4287f5"
-        ).grid(row=0, column=3, rowspan=2)
+        
 
-        tk.Label(mainFrame,
+        ttk.Label(mainFrame,
             text="Timeoversikt.csv"
         ).grid(row= 1, column=0)
 
@@ -62,7 +64,7 @@ class Hub_UI:
         btns = []
         for exportKey in csv_splitter.timeoversikt_exports :
             btns.append(
-                tk.Button(mainFrame,
+                ttk.Button(mainFrame,
                     text=csv_splitter.timeoversikt_exports[exportKey]['name'],
                     command= lambda i = csv_splitter.timeoversikt_exports[exportKey]: 
                         csv_splitter.reformat_timeoversikt(self.get_file_save_dir(), self.get_csv_file(), i),
@@ -75,13 +77,13 @@ class Hub_UI:
 
 
         curRow = 2
-        tk.Label(mainFrame,
+        ttk.Label(mainFrame,
             text="Prosjektstatus.csv"
         ).grid(row= 1, column=1)
 
         for exportKey in csv_splitter.prosjektstatus_exports :
             btns.append(
-                tk.Button(mainFrame,
+                ttk.Button(mainFrame,
                     text=csv_splitter.prosjektstatus_exports[exportKey]['name'],
                     command= lambda i = csv_splitter.prosjektstatus_exports[exportKey]: 
                         csv_splitter.reformat_prosjektstatus(self.get_file_save_dir(), self.get_csv_file(), i),
@@ -91,6 +93,13 @@ class Hub_UI:
             btns[len(btns) - 1].grid(row=curRow, column=1)
             tooltip.create(btns[len(btns) - 1], csv_splitter.prosjektstatus_exports[exportKey]['description'])
             curRow += 1
+
+        ttk.Button(mainFrame,
+            text="Velg prosjekt typer",
+            command=configurator.main,
+            width=30,
+            # bg="#4287f5"
+        ).grid(row=100, column=0, columnspan=2)
 
         root.mainloop()
 
--- tooltip.py
+++ tooltip.py
@@ -20,8 +20,8 @@ class ToolTip(object):
         tw.wm_overrideredirect(1)
         tw.wm_geometry("+%d+%d" % (x, y))
         label = Label(tw, text=self.text, justify=LEFT,
-                      background="#ffffe0", relief=SOLID, borderwidth=1,
-                      font=("tahoma", "8", "normal"))
+                      background="#f7ffff", relief=SOLID, borderwidth=1,
+                      font=("tahoma", "9", "normal"))
         label.pack(ipadx=1)
 
     def hidetip(self):
