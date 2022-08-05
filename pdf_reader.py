a12e4e90d639e213fcae1799fed02a5cca26d05c
Sindre Denby <sidenby1@gmail.com>
2022-08-05 13:56:40 +0200

    -

--- pdf_reader.py
+++ pdf_reader.py
@@ -4,7 +4,11 @@ def main():
     pdfFile = open("C:/Users/si-de/Downloads/Bilag-132252-2022.pdf", "rb")
     pdfReader = PyPDF2.PdfReader(pdfFile)
     page = pdfReader._get_page(0)
-    print(page.extract_text())
+    pageText = (page.extract_text())
+
+    for line in pageText.splitlines():
+        if "Sum debet" in line:
+            print(float("".join(line.split(" ")[2:]).replace(",", ".")))
 
 if __name__ == '__main__':
     main()
