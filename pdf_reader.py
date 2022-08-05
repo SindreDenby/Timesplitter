5f36fef5189cd4467f368988cfbd387fbdfe9974
Sindre Denby <sidenby1@gmail.com>
2022-08-05 13:31:14 +0200

    pdf

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,2 +1,10 @@
 import PyPDF2
 
+def main():
+    pdfFile = open("C:/Users/si-de/Downloads/Bilag-132252-2022.pdf", "rb")
+    pdfReader = PyPDF2.PdfReader(pdfFile)
+    page = pdfReader._get_page(0)
+    print(page.extract_text())
+
+if __name__ == '__main__':
+    main()
